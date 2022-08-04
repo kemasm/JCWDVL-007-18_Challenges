9c70be471ad7f19a044bc8bd1983265d86f8b85d
makse <maxx23295@gmail.com>
2022-08-04 19:50:54 +0700

    better space complexity

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,12 @@
 export function uppercaseWave(input_str) {
-  let outputs = [];
-  for (let idx = 0; idx < input_str.length; idx++) {
-    let wavingChar = input_str[idx].toUpperCase();
-    let left = input_str.slice(0, idx);
-    let right = input_str.slice(idx + 1);
-    let output = left + wavingChar + right;
-    outputs.push(output);
+  const outputs = [];
+  const splittedInput = Array.from(input_str);
+  for (let idx = 0; idx < splittedInput.length; idx++) {
+    splittedInput[idx] = splittedInput[idx].toUpperCase();
+    outputs.push(splittedInput.join(""));
+    splittedInput[idx] = splittedInput[idx].toLowerCase();
   }
-  outputs = outputs.join("\n");
-  return outputs;
+  return outputs.join("\n");
 }
 
 console.log(uppercaseWave("ryan"));
