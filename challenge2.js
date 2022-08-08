89f6b3d3f5f97ca03ac638c67ec7fc353443d0d7
makse <maxx23295@gmail.com>
2022-08-08 21:46:15 +0700

    space complexity optimization on challenges2

--- challenge2.js
+++ challenge2.js
@@ -10,15 +10,13 @@ function isOdd(num) {
 export function oddSliceOdd(inputs = []) {
   if (inputs.length < 2) return inputs;
 
-  let result = [];
   for (let idx = 0; idx < inputs.length - 1; idx++) {
-    result.push(inputs[idx]);
     if (isOdd(inputs[idx]) && isOdd(inputs[idx + 1])) {
-      result.push("-");
+      inputs.splice(idx + 1, 0, "-");
+      idx += 1;
     }
   }
-  result.push(inputs[inputs.length - 1]);
-  return result;
+  return inputs;
 }
 
 // const input = [3, 9, 2, 3, 4, 7, 5, 6];
