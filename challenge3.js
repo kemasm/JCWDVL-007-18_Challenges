0b612cc1d8cbcfa337272297d554faf40d19ae16
makse <maxx23295@gmail.com>
2022-08-09 22:03:48 +0700

    finish challenge 3

--- challenge3.js
+++ challenge3.js
@@ -1,5 +1,34 @@
-function echo(input) {
-  return input;
+// !DuplicateAlpha
+// Buatlah function untuk mencari huruf yang tidak terduplikasi
+// ex. input: 'Purwadhika'
+//      ouput: 'Huruf P, u, r, w, d, h, i, k masih original dan belum terduplikasi'
+const outputFmt = (...args) =>
+  `Huruf ${args[0]} masih original dan belum terduplikasi`;
+
+function notDuplicateAlpha(chars) {
+  input = chars.slice();
+  input = input.replace(/[^a-zA-Z]/gi, "");
+
+  const charDict = {};
+  for (let char of input) {
+    if (charDict[char.toUpperCase()] || 0) char = char.toUpperCase();
+    if (charDict[char.toLowerCase()] || 0) char = char.toLowerCase();
+    charDict[char] = (charDict[char] || 0) + 1;
+  }
+
+  let results = Object.entries(charDict)
+    .filter((x) => x[1] == 1)
+    .map((x) => x[0]);
+
+  return results;
 }
 
-export { echo };
+let input = "Purwadhika";
+let result = notDuplicateAlpha(input).join(", ");
+console.log(outputFmt(result));
+
+input = "Hello world!";
+result = notDuplicateAlpha(input).join(", ");
+console.log(outputFmt(result));
+
+export { notDuplicateAlpha };
