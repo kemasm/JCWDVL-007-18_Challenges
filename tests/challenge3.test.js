23ee4e1c18fc66f3704c44c260b449df4b32f7a1
makse <maxx23295@gmail.com>
2022-08-10 19:16:22 +0700

    pass challenge 3

--- tests/challenge3.test.js
+++ tests/challenge3.test.js
@@ -2,10 +2,66 @@ import { Test } from "../utils/test_utils.js";
 import * as module from "../challenge3.js";
 
 export class Challenge3Test extends Test {
+  test_sanity() {
+    const input = "";
+    const expectedOutput = [];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_base_case() {
+    const input = "a";
+    const expectedOutput = ["a"];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_base_case2() {
+    const input = "ab";
+    const expectedOutput = ["a", "b"];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_base_case3() {
+    const input = "aab";
+    const expectedOutput = ["b"];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_base_case4() {
+    const input = "aabb";
+    const expectedOutput = [];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_base_case4() {
+    const input = "Abb";
+    const expectedOutput = ["A"];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_base_case4() {
+    const input = "Aab";
+    const expectedOutput = ["b"];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
   test_given_test_case() {
-    const input = "bar";
-    const expectedOutput = "bar";
-    const result = module.echo(input);
+    const input = "Purwadhika";
+    const expectedOutput = ["P", "u", "r", "w", "d", "h", "i", "k"];
+    const result = module.notDuplicateAlpha(input);
+    this.assertEqual(expectedOutput, result);
+  }
+
+  test_given_test_case2() {
+    const input = "Hello world!";
+    const expectedOutput = "Hewrd".split("");
+    const result = module.notDuplicateAlpha(input);
     this.assertEqual(expectedOutput, result);
   }
 }
