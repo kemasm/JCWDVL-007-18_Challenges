fa392015ca50396deeb6e754b32d0c968f78a040
makse <maxx23295@gmail.com>
2022-08-17 23:54:31 +0700

    init branch

---
+++ index.js
@@ -0,0 +1,31 @@
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.table(matrix);
+
+function mirror(matrix) {
+  return matrix.map((arr) => arr.reverse());
+}
+
+function transpose(matrix) {
+  let newMatrix = [];
+  for (let idx = 0; idx < matrix.length; idx++) {
+    let newArr = [];
+    for (let idx2 = 0; idx2 < matrix.length; idx2++) {
+      newArr.push(matrix[idx2][idx]);
+    }
+    newMatrix.push(newArr);
+  }
+  return newMatrix;
+}
+
+function rotate(matrix, direction) {
+  if (direction === "left") return transpose(mirror(matrix));
+  else return mirror(transpose(matrix));
+}
+
+matrix = rotate(matrix, "left");
+fnDrawTd();
